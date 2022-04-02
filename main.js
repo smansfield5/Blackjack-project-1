0d6c3f1e77c7b724fd46aa0020813c2e74d4b25b
smansfield5 <smansfield5@yahoo.com>
2022-04-02 17:29:22 -0600

    background

---
+++ main.js
@@ -0,0 +1,31 @@
+/*----- constants -----*/
+let player = [];
+let dealer = [];
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+
+
+/*----- app's state (variables) -----*/
+let bank; //An object with the players money amount to play with
+let results; //Determines which hand wins
+let winner; // Checks 
+
+/*----- cached element references -----*/
+cards = [];
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+init()
+function init() {
+    player.push()
+    dealer.push()
+
+    render()
+}
+
+// render() {
+//     //renderHand()
+//     //renderBank()
+// };
