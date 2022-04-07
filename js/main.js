99bfb0e45982f1cae30be38ef96e9d1ba6973f12
smansfield5 <smansfield5@yahoo.com>
2022-04-07 10:55:46 -0700

    message displayed, still needs work

--- js/main.js
+++ js/main.js
@@ -6,13 +6,13 @@ const masterDeck = buildMasterDeck();
 const newDeck = getNewShuffledDeck()
 
 const player = {
-    playerHand: [],
-    bank: 3000
+  playerHand: [],
+  bank: 3000
 
 }
 
 const dealer = {
-    dealerHand: [],
+  dealerHand: [],
 }
 /*----- app's state (variables) -----*/
 //let bank = 2000; //An object with the players money amount to play with
@@ -34,6 +34,7 @@ const hitButton = document.getElementById('hit');
 const stayButton = document.getElementById('stay');
 const chipButton1 = document.getElementById('chips1')
 const chipButton2 = document.getElementById('chips2')
+const msgEl = document.getElementById('msg')
 
 /*----- event listeners -----*/
 hitButton.addEventListener('click', hit);
@@ -46,55 +47,37 @@ chipButton2.addEventListener('click', redBet)
 init();
 function init() {
   staying = false;
-  
-    render()
+
+  render()
 }
 
 function render() {
-    getNewShuffledDeck();
-    renderGame();
-    renderHand();
-    total()
-    determineWinner()
+  getNewShuffledDeck();
+  renderGame();
+  renderHand();
+  total()
+
+  determineWinner()
 }
 
 function renderGame() {
-    
+
 }
 
 function renderHand() {
   clearHands()
-  player.playerHand.forEach(function(card) {
+  player.playerHand.forEach(function (card) {
     const cardEl = document.createElement('div');
     cardEl.className = `card ${card.face}`
     playerCards.appendChild(cardEl)
-})
-  dealer.dealerHand.forEach(function(card, index) {
+  })
+  dealer.dealerHand.forEach(function (card, index) {
     const cardEl = document.createElement('div');
     cardEl.className = index === 0 || staying === true ? `card ${card.face}` : `card back`
     dealerCards.appendChild(cardEl)
-});
+  });
 
 }
-
-
-
-// function determineWinner() {
-  
-//     if (playerTotal === dealerTotal) {
-//         console.log('Draw')
-//     } else if (playerTotal === 21 || dealerTotal === 21) {
-//         console.log('Blackjack!')
-//     } else if (playerTotal > 21 || dealerTotal > 21) {
-//       console.log('Bust')
-//     } else if (playerTotal > dealerTotal) {
-//       console.log('Player Wins!')
-//     } else if (playerTotal < dealerTotal) {
-//       console.log('Dealer Wins!')
-//     } else {
-//       console.log('not working')
-//     }
-//   }
 
 // function payOut() {
 //   if (player)
@@ -102,117 +85,119 @@ function renderHand() {
 
 function redBet() {
   player.bank -= redChip;
-    bank.innerHTML = player.bank; 
-    
+  bank.innerHTML = player.bank;
+
 }
 
 function yellowBet() {
   player.bank -= yellowChip;
-    bank.innerHTML = player.bank
-    
-    
+  bank.innerHTML = player.bank
+
+
 }
 
 
 
 function deal() {
-    playerCard = newDeck.pop();
-    player.playerHand.push(playerCard);
-    dealerCard = newDeck.pop();
-    dealer.dealerHand.push(dealerCard)
-    playerCard = newDeck.pop();
-    player.playerHand.push(playerCard);
-    dealerCard = newDeck.pop();
-    dealer.dealerHand.push(dealerCard)
-    // console.log('player')
-    // console.log(player.playerHand)
-    // console.log('dealer')
-    // console.log(dealer.dealerHand)
-    dealButton.style.visibility = "hidden";
-    chipButton2.style.visibility = "hidden";
-    chipButton1.style.visibility = "hidden";
-    render()
+  playerCard = newDeck.pop();
+  player.playerHand.push(playerCard);
+  dealerCard = newDeck.pop();
+  dealer.dealerHand.push(dealerCard)
+  playerCard = newDeck.pop();
+  player.playerHand.push(playerCard);
+  dealerCard = newDeck.pop();
+  dealer.dealerHand.push(dealerCard)
+  // console.log('player')
+  // console.log(player.playerHand)
+  // console.log('dealer')
+  // console.log(dealer.dealerHand)
+  dealButton.style.visibility = "hidden";
+  chipButton2.style.visibility = "hidden";
+  chipButton1.style.visibility = "hidden";
+  render()
 }
 
 function hit() {
-   clearHands()
-    playerCard = newDeck.pop();
-    player.playerHand.push(playerCard);
-    player.playerHand.forEach(function(card) {
-      const cardEl = document.createElement('div');
-      cardEl.className = `card ${card.face}`
-      playerCards.appendChild(cardEl)
-      //console.log(player.playerHand)
+  clearHands()
+  playerCard = newDeck.pop();
+  player.playerHand.push(playerCard);
+  player.playerHand.forEach(function (card) {
+    const cardEl = document.createElement('div');
+    cardEl.className = `card ${card.face}`
+    playerCards.appendChild(cardEl)
+    //console.log(player.playerHand)
+
   })
-    render();
-  
+  render();
+
 }
 
 function clearHands() {
-  Array.from(playerCards.children).forEach(function(card) {
+  Array.from(playerCards.children).forEach(function (card) {
     playerCards.removeChild(card);
   })
-  Array.from(dealerCards.children).forEach(function(card) {
+  Array.from(dealerCards.children).forEach(function (card) {
     dealerCards.removeChild(card);
 
-  }) 
- }
-
-
-
-function dealerHit() {
- if (dealerCards < 17){
-    dealerCard = newDeck.pop();
-    dealer.dealerHand.push(dealerCard)
-  } else {
-    staying = true
-  }
-  render()
+  })
 }
 
 function stay() {
   staying = true;
-  dealerHit()
-  //determineWinner()
   render();
-  //console.log(dealerTotal)
-  //console.log(determineWinner)
+
 }
 
-function  total() {
+function dealerHit() {
+
+  if (dealer.dealerHand < 17) {
+    dealerCard = newDeck.pop();
+    dealer.dealerHand.push(dealerCard)
+  } else {
+    dealerHit()
+    staying = true
+  }
+
+}
+
+
+
+function total() {
   playerTotal = 0
   for (let p = 0; p < player.playerHand.length; p++) {
-  playerTotal += player.playerHand[p].value;
-  }    
+    playerTotal += player.playerHand[p].value;
+  }
   console.log('player')
   console.log(playerTotal)
   dealerTotal = 0;
-for (let d = 0; d < dealer.dealerHand.length; d++) {
-  dealerTotal += dealer.dealerHand[d].value;
-} 
-console.log('dealer')
+  for (let d = 0; d < dealer.dealerHand.length; d++) {
+    dealerTotal += dealer.dealerHand[d].value;
+  }
+  console.log('dealer')
   console.log(dealerTotal)
-  
+
 }
 
 function determineWinner() {
-  
+
   if (playerTotal === dealerTotal) {
-    console.log('Draw')
-} else if (playerTotal === 21 || dealerTotal === 21) {
-    console.log('Blackjack!')
-} else if (playerTotal > 21) {
-  console.log('Player Bust')
-} else if (dealerTotal > 21) {
-  console.log('Dealer Bust')
-} else if (playerTotal > dealerTotal) {
-  console.log('Player Wins!')
-} else if (playerTotal < dealerTotal) {
-  console.log('Dealer Wins!')
-} else {
-  console.log('not working')
+    msgEl.innerText = 'Draw'
+  } else if (playerTotal === 21) {
+    msgEl.innerText = 'Player Blackjack!'
+  } else if (dealerTotal === 21) {
+    msgEl.innerText ='dealer Blackjack'
+  } else if (playerTotal > 21) {
+    msgEl.innerText = 'bust'
+  } else if (dealerTotal > 21) {
+    msgEl.innerText = 'Dealer Bust'
+  } else if (playerTotal > dealerTotal) {
+    msgEl.innerText = 'Player Wins!'
+  } else if (playerTotal < dealerTotal) {
+    msgEl.innerText = 'Dealer Wins!'
+  } else {
+    msgEl.innerText = 'not working'
   }
-  
+
 }
 
 
@@ -255,8 +240,8 @@ function renderNewShuffledDeck() {
 function buildMasterDeck() {
   const deck = [];
   // Use nested forEach to generate card objects
-  suits.forEach(function(suit) {
-    ranks.forEach(function(rank) {
+  suits.forEach(function (suit) {
+    ranks.forEach(function (rank) {
       deck.push({
         // The 'face' property maps to the library's CSS classes for cards
         face: `${suit}${rank}`,
